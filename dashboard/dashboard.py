51776cb0abc559540ed296b0b9b0b8c56b1404a6
jessicas16 <jessica.susanto16@gmail.com>
2024-10-01 15:12:55 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,7 +6,6 @@ import streamlit as st
 import urllib
 from babel.numbers import format_currency
 sns.set(style='dark')
-# st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 class DataAnalyzer:
@@ -77,7 +76,7 @@ class BrazilMapPlotter:
         self.st = st
 
     def plot(self):
-        brazil = self.mpimg.imread(self.urllib.request.urlopen('https://i.pinimg.com/originals/3a/0c/e1/3a0ce18b3c842748c255bc0aa445ad41.jpg'),'jpg')
+        brazil = self.mpimg.imread(self.urllib.request.urlopen('https://img.freepik.com/premium-vector/brazil-map_391639-1338.jpg'),'jpg')
         ax = self.data.plot(kind="scatter", x="geolocation_lng", y="geolocation_lat", figsize=(10,10), alpha=0.3,s=0.3,c='maroon')
         self.plt.axis('off')
         self.plt.imshow(brazil, extent=[-73.98283055, -33.8,-33.75116944,5.4])
