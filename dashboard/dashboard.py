da0bf7f81452f082d522b499b45f003a7b1f07d8
jessicas16 <jessica.susanto16@gmail.com>
2024-10-01 17:34:01 +0700

    ubah all

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,241 +2,79 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import seaborn as sns
-import streamlit as st
 import urllib
+import streamlit as st
 from babel.numbers import format_currency
 sns.set(style='dark')
 
-
-class DataAnalyzer:
-    def __init__(self, df):
-        self.df = df
-
-    def create_daily_orders_df(self):
-        daily_orders_df = self.df.resample(rule='D', on='order_approved_at').agg({
-            "order_id": "nunique",
-            "payment_value": "sum"
-        })
-        daily_orders_df = daily_orders_df.reset_index()
-        daily_orders_df.rename(columns={
-            "order_id": "order_count",
-            "payment_value": "revenue"
-        }, inplace=True)
-        
-        return daily_orders_df
-    
-    def create_sum_spend_df(self):
-        sum_spend_df = self.df.resample(rule='D', on='order_approved_at').agg({
-            "payment_value": "sum"
-        })
-        sum_spend_df = sum_spend_df.reset_index()
-        sum_spend_df.rename(columns={
-            "payment_value": "total_spend"
-        }, inplace=True)
-
-        return sum_spend_df
-
-    def create_sum_order_items_df(self):
-        sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index()
-        sum_order_items_df.rename(columns={
-            "product_id": "product_count"
-        }, inplace=True)
-        sum_order_items_df = sum_order_items_df.sort_values(by='product_count', ascending=False)
-
-        return sum_order_items_df
-
-    def review_score_df(self):
-        review_scores = self.df['review_score'].value_counts().sort_values(ascending=False)
-        most_common_score = review_scores.idxmax()
-
-        return review_scores, most_common_score
-
-    def create_bystate_df(self):
-        bystate_df = self.df.groupby(by="customer_state").customer_id.nunique().reset_index()
-        bystate_df.rename(columns={
-            "customer_id": "customer_count"
-        }, inplace=True)
-        most_common_state = bystate_df.loc[bystate_df['customer_count'].idxmax(), 'customer_state']
-        bystate_df = bystate_df.sort_values(by='customer_count', ascending=False)
-
-        return bystate_df, most_common_state
-
-    def create_order_status(self):
-        order_status_df = self.df["order_status"].value_counts().sort_values(ascending=False)
-        most_common_status = order_status_df.idxmax()
-
-        return order_status_df, most_common_status
-    
-class BrazilMapPlotter:
-    def __init__(self, data, plt, mpimg, urllib, st):
-        self.data = data
-        self.plt = plt
-        self.mpimg = mpimg
-        self.urllib = urllib
-        self.st = st
-
-    def plot(self):
-        brazil = self.mpimg.imread(self.urllib.request.urlopen('https://img.freepik.com/premium-vector/brazil-map_391639-1338.jpg'),'jpg')
-        ax = self.data.plot(kind="scatter", x="geolocation_lng", y="geolocation_lat", figsize=(10,10), alpha=0.3,s=0.3,c='maroon')
-        self.plt.axis('off')
-        self.plt.imshow(brazil, extent=[-73.98283055, -33.8,-33.75116944,5.4])
-        self.st.pyplot()
-
-# Dataset
-datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-all_df = pd.read_csv("./data/all_data.csv")
-all_df.sort_values(by="order_approved_at", inplace=True)
-all_df.reset_index(inplace=True)
-
-# Geolocation Dataset
-geolocation = pd.read_csv('./data/geolocation.csv')
-data = geolocation.drop_duplicates(subset='customer_unique_id')
-
-for col in datetime_cols:
-    all_df[col] = pd.to_datetime(all_df[col])
-
-min_date = all_df["order_approved_at"].min()
-max_date = all_df["order_approved_at"].max()
+# Menampilkan judul aplikasi
+st.title("Dashboard Analisis Data E-Commerce")
 
 # Sidebar
 with st.sidebar:
     # Title
+    st.title("Submission Dicoding")
     st.title("Jessica Susanto")
 
-    # Date Range
-    start_date, end_date = st.date_input(
-        label="Select Date Range",
-        value=[min_date, max_date],
-        min_value=min_date,
-        max_value=max_date
-    )
+# Memuat dataset
+customers_df = pd.read_csv('./dataset/customers_dataset.csv')
+geo_df = pd.read_csv('./dataset/geolocation_dataset.csv')
+order_items = pd.read_csv('./dataset/order_items_dataset.csv')
+order_pay = pd.read_csv('./dataset/order_payments_dataset.csv')
+order_rev = pd.read_csv('./dataset/order_reviews_dataset.csv')
+orders_df = pd.read_csv('./dataset/orders_dataset.csv')
+product_cat = pd.read_csv('./dataset/product_category_name_translation.csv')
+products_df = pd.read_csv('./dataset/products_dataset.csv')
+sellers_df = pd.read_csv('./dataset/sellers_dataset.csv')
 
-# Main
-main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) & 
-                 (all_df["order_approved_at"] <= str(end_date))]
+# Menampilkan visualisasi Tren Penjualan dari waktu ke waktu
+st.subheader("Tren Penjualan dari Waktu ke Waktu")
 
-function = DataAnalyzer(main_df)
-map_plot = BrazilMapPlotter(data, plt, mpimg, urllib, st)
+# Convert the order_purchase_timestamp to datetime format
+orders_df['order_purchase_timestamp'] = pd.to_datetime(orders_df['order_purchase_timestamp'])
 
-daily_orders_df = function.create_daily_orders_df()
-sum_spend_df = function.create_sum_spend_df()
-sum_order_items_df = function.create_sum_order_items_df()
-review_score, common_score = function.review_score_df()
-state, most_common_state = function.create_bystate_df()
-order_status, common_status = function.create_order_status()
+# Extract year and month for grouping
+orders_df['year_month'] = orders_df['order_purchase_timestamp'].dt.to_period('M')
 
-# Title
-st.header("E-Commerce Dashboard :convenience_store:")
+# Group by year and month to count the number of orders
+sales_trend = orders_df.groupby('year_month').size()
 
-# Daily Orders
-st.subheader("Daily Orders")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    total_order = daily_orders_df["order_count"].sum()
-    st.markdown(f"Total Order: **{total_order}**")
-
-with col2:
-    total_revenue = format_currency(daily_orders_df["revenue"].sum(), "IDR", locale="id_ID")
-    st.markdown(f"Total Revenue: **{total_revenue}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(
-    daily_orders_df["order_approved_at"],
-    daily_orders_df["order_count"],
-    marker="o",
-    linewidth=2,
-    color="#90CAF9"
-)
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
+# Plot the trend of orders over time
+fig, ax = plt.subplots(figsize=(10, 6))
+sales_trend.plot(ax=ax)
+ax.set_title('Tren Penjualan dari Waktu ke Waktu')
+ax.set_xlabel('Waktu (Year-Month)')
+ax.set_ylabel('Jumlah Pesanan')
+ax.grid(True)
+ax.tick_params(axis='x', rotation=45)
 st.pyplot(fig)
 
-# Customer Spend Money
-st.subheader("Customer Spend Money")
-col1, col2 = st.columns(2)
+# Menampilkan visualisasi Metode Pembayaran
+st.subheader("Distribusi Metode Pembayaran")
 
-with col1:
-    total_spend = format_currency(sum_spend_df["total_spend"].sum(), "IDR", locale="id_ID")
-    st.markdown(f"Total Spend: **{total_spend}**")
+# Group by payment_type to count the number of payments for each method
+payment_type_distribution = order_pay['payment_type'].value_counts()
 
-with col2:
-    avg_spend = format_currency(sum_spend_df["total_spend"].mean(), "IDR", locale="id_ID")
-    st.markdown(f"Average Spend: **{avg_spend}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(
-    sum_spend_df["order_approved_at"],
-    sum_spend_df["total_spend"],
-    marker="o",
-    linewidth=2,
-    color="#90CAF9"
-)
-ax.tick_params(axis="x", rotation=45)
-ax.tick_params(axis="y", labelsize=15)
+# Plot the distribution of payment types
+fig, ax = plt.subplots(figsize=(8, 6))
+payment_type_distribution.plot(kind='bar', ax=ax)
+ax.set_title('Distribusi Metode Pembayaran')
+ax.set_xlabel('Metode Pembayaran')
+ax.set_ylabel('Jumlah Penggunaan')
+ax.grid(True)
 st.pyplot(fig)
 
-# Order Items
-st.subheader("Order Items")
-col1, col2 = st.columns(2)
+# Menampilkan visualisasi Distribusi Ulasan Pelanggan
+st.subheader("Distribusi Skor Ulasan Pelanggan")
 
-with col1:
-    total_items = sum_order_items_df["product_count"].sum()
-    st.markdown(f"Total Items: **{total_items}**")
+# Group by review_score to count the number of reviews for each score
+review_score_distribution = order_rev['review_score'].value_counts().sort_index()
 
-with col2:
-    avg_items = sum_order_items_df["product_count"].mean()
-    st.markdown(f"Average Items: **{avg_items}**")
-
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
-
-colors = ["#068DA9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel("Number of Sales", fontsize=30)
-ax[0].set_title("Produk paling banyak terjual", loc="center", fontsize=50)
-ax[0].tick_params(axis ='y', labelsize=35)
-ax[0].tick_params(axis ='x', labelsize=30)
-
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette=colors, ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel("Number of Sales", fontsize=30)
-ax[1].invert_xaxis()
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].set_title("Produk paling sedikit terjual", loc="center", fontsize=50)
-ax[1].tick_params(axis='y', labelsize=35)
-ax[1].tick_params(axis='x', labelsize=30)
-
+# Plot the distribution of review scores
+fig, ax = plt.subplots(figsize=(8, 6))
+review_score_distribution.plot(kind='bar', ax=ax)
+ax.set_title('Distribusi Skor Ulasan Pelanggan')
+ax.set_xlabel('Skor Ulasan')
+ax.set_ylabel('Jumlah Ulasan')
+ax.grid(True)
 st.pyplot(fig)
-
-# Review Score
-st.subheader("Review Score")
-col1,col2 = st.columns(2)
-
-with col1:
-    avg_review_score = review_score.mean()
-    st.markdown(f"Average Review Score: **{avg_review_score}**")
-
-with col2:
-    most_common_review_score = review_score.value_counts().index[0]
-    st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
-
-fig, ax = plt.subplots(figsize=(12, 6))
-sns.barplot(x=review_score.index, 
-            y=review_score.values, 
-            order=review_score.index,
-            palette=["#068DA9" if score == common_score else "#D3D3D3" for score in review_score.index]
-            )
-
-plt.title("Rating by customers for service", fontsize=15)
-plt.xlabel("Rating")
-plt.ylabel("Count")
-plt.xticks(fontsize=12)
-st.pyplot(fig)
-
-# Customer Demographic
-st.subheader("Customer Demographic")
-map_plot.plot()
